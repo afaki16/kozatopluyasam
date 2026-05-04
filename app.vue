a00f429f819f92885284d567d27b55adc2db747a
unknown <hasan_gulec96@outlook.com>
2026-05-04 22:52:28 +0300

    added ga4

--- app.vue
+++ app.vue
@@ -1,3 +1,22 @@
+<script setup>
+useHead({
+  script: [
+    {
+      async: true,
+      src: 'https://www.googletagmanager.com/gtag/js?id=G-NLD655BP4D'
+    },
+    {
+      children: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-NLD655BP4D');
+      `
+    }
+  ]
+})
+</script>
+
 <template>
   <NuxtPage />
 </template>
